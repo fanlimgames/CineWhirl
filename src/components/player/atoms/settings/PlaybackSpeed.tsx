a2cec2470c6b60f7c6f72133cf74fbbfde6ae6dd
fanlim <128323567+fanlimgames@users.noreply.github.com>
2024-08-19 17:40:15 +0200

    added more speed options

--- src/components/player/atoms/settings/PlaybackSpeed.tsx
+++ src/components/player/atoms/settings/PlaybackSpeed.tsx
@@ -6,19 +6,22 @@ import { Menu } from "@/components/player/internals/ContextMenu";
 import { useOverlayRouter } from "@/hooks/useOverlayRouter";
 import { usePlayerStore } from "@/stores/player/store";
 
+// Updated playback speeds
+const options = [0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 3, 4];
+
 function ButtonList(props: {
   options: number[];
   selected: number;
-  onClick: (v: any) => void;
+  onClick: (v: number) => void;
 }) {
   return (
-    <div className="flex items-center bg-video-context-buttons-list p-1 rounded-lg">
-      {props.options.map((option) => {
-        return (
+    <div className="overflow-x-auto whitespace-nowrap bg-video-context-buttons-list p-1 rounded-lg">
+      <div className="flex items-center">
+        {props.options.map((option) => (
           <button
             type="button"
             className={classNames(
-              "w-full px-2 py-1 rounded-md tabbable",
+              "inline-block px-2 py-1 rounded-md tabbable",
               props.selected === option
                 ? "bg-video-context-buttons-active text-white"
                 : null,
@@ -28,13 +31,13 @@ function ButtonList(props: {
           >
             {option}x
           </button>
-        );
-      })}
+        ))}
+      </div>
     </div>
   );
 }
 
-export function PlaybackSettingsView({ id }: { id: string }) {
+export function PlaybackSpeed({ id }: { id: string }) {
   const { t } = useTranslation();
   const router = useOverlayRouter(id);
   const playbackRate = usePlayerStore((s) => s.mediaPlaying.playbackRate);
@@ -46,8 +49,6 @@ export function PlaybackSettingsView({ id }: { id: string }) {
     },
     [display],
   );
-
-  const options = [0.25, 0.5, 1, 1.5, 2];
 
   return (
     <>
